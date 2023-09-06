487d2ebce769dff4f44b3df1b5118e965e988477
Prerana <prerana@hellosivi.com>
2023-09-06 06:57:25 +0530

    decoder part of the transformer

--- model.py
+++ model.py
@@ -137,3 +137,29 @@ class Encoder(nn.Module):
             x = layer(x, src_mask)
         return self.layer_norm(x)
     
+
+class DecoderBlock(nn.Module):
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
+
+    def forward(self, x, src_mask, tgt_mask, encoder_output):
+        x = self.residual_connections[0](x, self.self_attention_block(x, x, x, tgt_mask))
+        x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connections[2](x, self.feed_forward_block(x))
+        return x
+    
+class Decoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.layer_norm = LayerNormalization()
+
+    def forward(self, x, src_mask, tgt_mask, encoder_output):
+        for layer in self.layers:
+            x = layer(x, src_mask, tgt_mask, encoder_output)
+        return self.layer_norm(x)
+    
