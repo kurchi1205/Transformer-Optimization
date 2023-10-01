05780b01774447e3dd16c94940b8b494543c18ac
prerana <prerana.chaks@gmail.com>
2023-10-01 17:52:24 +0000

    validation bug fixes

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_
         if (decoder_input.size(1) == max_len):
             break
 
-        decoder_output = model.decode(encoder_output, source_mask, decoder_input, causal_mask(decoder_input.size(1).type_as(source_mask).to(device)))
+        decoder_output = model.decode(encoder_output, source_mask, decoder_input, causal_mask(decoder_input.size(1)).type_as(source_mask).to(device))
         prob = model.project(decoder_output[:, -1])
         _, next_token = torch.max(prob, dim=-1)
         decoder_input = torch.cat([decoder_input, torch.empty(1, 1).type_as(source).fill_(next_token.item())], dim=1)
@@ -188,7 +188,7 @@ def train_model(config):
             optimizer.zero_grad(set_to_none=True)
             global_step+=1
 
-        run_validation(model, val_loader, tokenizer_tgt, config['seq_len'], device, None, None)
+        run_validation(model, val_loader, tokenizer_src, tokenizer_tgt, config['seq_len'], device, None, None)
 
         model_filename = get_weight_file_path(config, str(epoch))
         torch.save({
